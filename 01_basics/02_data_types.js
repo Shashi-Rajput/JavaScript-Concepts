7c3754db450b4d19bfd76b1702781f1117ccb532
Shashi Rajput <143606950+Shashi-Rajput@users.noreply.github.com>
2023-09-02 10:31:22 +0000

    Data types in Java script.

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,30 @@
+//  JavaScript has * types of data:-
+
+
+
+/* 1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+*/
+
+let a = "hy";
+let b = 12214242;
+let c = 4523532456; // means above 2*10 to power 53
+let d = true;
+let e;
+let f = null;
+let  symbol // it is use to specially in react or figma tools ot find uniqueness of components.
+let object  // class is the templete of objects object is the instance of class.
+
+
+
+//   NOTES :--
+
+//  In console.log we have different value to find data, function used to find datatype is =
+console.log(typeof "");
+// NOTE - For Undefined it shows undefined and for null this function shows object data type.
