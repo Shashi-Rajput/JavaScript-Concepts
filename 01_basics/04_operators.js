954cd04ef0fe4cda9412d376858bcdffdf293b89
Shashi Rajput <143606950+Shashi-Rajput@users.noreply.github.com>
2023-09-02 12:28:55 +0000

    operators in java script.

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,60 @@
+//  OPERATORS
+//  1. Arithmatic  Opreators
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1*1);
+console.log(1%1);
+console.log(1**1);   //  It means power
+
+//  Note:- In javascript '+' is used to concatenate and in php '.' sign is used to concatenate.
+//  2. Assignment Opreators
+console.log(1=2);
+console.log(1-=1);
+console.log(1+=1);
+console.log(1*=1);
+
+
+//  3. Comparison Opreators
+console.log(1==1);
+console.log(1<2);
+console.log(1>2);
+console.log(1>=2);
+console.log(1<=2);
+
+//  4. Logical Opreators
+console.log(1&&1); // or AND
+console.log(1||1);  // or OR
+console.log(1!=2 );  //  not equal to
+
+
+
+
+// console.log("harry");
+//  harry
+// console.log("harry" + "hello");
+//  harryhello
+
+// console.log("hary " + " hello");
+//  hary  hello
+
+// console.log("harry"+ " hello");
+//  harry hello
+
+// console.log("12 " + " 12");
+//  12  12
+
+// console.log("12" + 12);
+//  1212
+
+// console.log(12 + 12);
+
+
+//  Note:- Some Interesting Facts,
+
+
+conslole.log("2"+2+2);
+//  Note :-  It's output is 222 
+
+conslole.log(2+2+"2");
+//  Note :-  It's output is 42.
