3ae2a2a1d5d279a138e4eb4f7310e87f00dab23e
Shashi Rajput <143606950+Shashi-Rajput@users.noreply.github.com>
2023-09-02 11:14:46 +0000

    data cnversions in java script.

---
+++ 01_basics/03_data_conversions.js
@@ -0,0 +1,7 @@
+let num = 12;
+console.log(typeof num);
+let connum = Number(num);
+console.log(connum);
+console.log(typeof connum);
+
+//   Note - Nan( full form of nam is not a number). Nan comes after console.log(typeof "dfd");
