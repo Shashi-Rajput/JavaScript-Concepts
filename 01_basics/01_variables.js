68c0d3fa751cbf5d337a7642d38a75d53dbe16b9
Shashi Rajput <143606950+Shashi-Rajput@users.noreply.github.com>
2023-09-02 10:33:16 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const a = "hy";   // this can not be taken again.
+let b = "by";  // variable can't assign again but it's value can be assigned again.
+var c = "no"; // This can be assignes agained again also it's value can be change again.
+d = "yes";   // this is also a variable in in javaScript. 
+e ; // this is also a variable, but this is called = undefined.
+
+//  Note: In variables "" is necessary, without this it shows error. 
+//  Note: at console.log if data is typed, then for string "" is necessary. But for variable no need to type "".
+
+//  Many methods to print the data in javaScript.
+//   1. console.log (""); this is used for string.
+//   2. console.log ( );  this is used for variables.
+//   3. console.log table([a,b,c,d,f] );   this is used for print multiple data in console in the form of table.
